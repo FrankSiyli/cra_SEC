66ba2d6e4f3b5b41fa72b469f9a79597ec6e38fe
Frank Siyli <frank.siyli@gmail.com>
2023-12-08 00:45:16 +0100

    style changes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -37,7 +37,20 @@ const NavBar = () => {
           {!showHamburgerMenu ? (
             <img className="hamburger" src={hamburger} alt="hamburger menu" />
           ) : (
-            <p className="x-hamburger-menu">X</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 x-hamburger-menu"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
           )}
         </button>
         {showHamburgerMenu && (
